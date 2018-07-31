8732874420541df43b0a04a898bc064c8042b7a4
Baddam <baddam1989@gmail.com>
2018-07-31 10:49:44 +0530

    Introducing Java-Learning project to Git

---
+++ src/main/java/math/CalculateNumbers.java
@@ -0,0 +1,13 @@
+package math;
+
+public class CalculateNumbers {
+
+	public static void main(String[] args) {
+		int result = sumNumbers(1, 2);
+		System.out.println("result :" + result);
+	}
+
+	public static int sumNumbers(int a, int b) {
+		return a + b;
+	}
+}
